e3f266aa01589293b465214b8f3bd159cbf4613f
Puffer <pufferdev0807@gmail.com>
2023-05-28 18:17:05 +0900

    update verify script

--- scripts/DevelopmentArgs.js
+++ scripts/DevelopmentArgs.js
@@ -1,5 +1,5 @@
-const shareHolderDao = "0xA2a0b8cF242DAa4D90178430A58D1F054B85d053";
-const productDao = "0x195dcCBEBAB5256eC8c6D8c719b512Cb4C2e342e";
-const staking = "0x335D9115453bfAb9982e2563D653409d051BD828";
+const shareHolderDao = "0xCF650d0202bF6ad5e045154F3A73801C535140b1";
+const productDao = "0x0e216E0dB3b618Fd318E673cD8897Bc5d16D17d5";
+const staking = "0x37750D58a777b8c611bF23147b625c2E96446E02";
 
 module.exports = [shareHolderDao, productDao, staking];
--- scripts/ProductArgs.js
+++ scripts/ProductArgs.js
@@ -1,3 +1,3 @@
-const stakingAddress = "0x335D9115453bfAb9982e2563D653409d051BD828";
+const stakingAddress = "0x37750D58a777b8c611bF23147b625c2E96446E02";
 
 module.exports = [stakingAddress];
